0e3eef85ca9fb37c69c50aa4800ff51b58147103
Artem <golynets04@gmail.com>
2023-02-02 02:19:35 +0300

    create Outlet

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    @IBOutlet weak var trueButton: UIButton!
+    
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    @IBOutlet weak var falseButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    @IBAction func clickButton(_ sender: UIButton) {
+        
+    }
+    
 
 }
 
